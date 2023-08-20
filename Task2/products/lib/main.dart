a176899d979a626b985f347f7e423195298fb0c0
Salma Youssef <syoussef4444@gmail.com>
2023-08-20 14:17:15 +0300

    updated task 2

--- Task2/products/lib/main.dart
+++ Task2/products/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'product.dart';
 import 'card.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/cupertino.dart';
 
 
 void main() {
@@ -16,35 +17,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return   MaterialApp(
       home:
-        Scaffold(
+      Scaffold(
         appBar: AppBar(
           title: Text("Shopping app"),
           centerTitle: true,
         ),
         body:
-            Container(
-    child: SingleChildScrollView(
-    child: Padding(
-    padding: const EdgeInsets.all(10.0),
+        Container(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
               child : Wrap (
                 spacing: 10,
                 runSpacing: 10,
-                  children: [
-                    ...products.map((product)=> card(product: product)).toList(),],
+                children: [
+                  ...products.map((product)=> card(product: product)).toList(),],
 
               ),
 
 
 
-              ),
+            ),
 
-                ),
-
-
+          ),
 
 
-    ),
+
+
         ),
+      ),
     );
   }
 }
